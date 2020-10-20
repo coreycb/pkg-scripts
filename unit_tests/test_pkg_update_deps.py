8d98a54a3a3589e7b72064a2c9baf3138460f573
Chris MacNaughton <chris.macnaughton@canonical.com>
2020-10-20 16:38:56 +0200

    Add more tests

--- unit_tests/test_pkg_update_deps.py
+++ unit_tests/test_pkg_update_deps.py
@@ -31,3 +31,41 @@ class UpdateDepsTests(unittest.TestCase):
         mock_open.assert_called()
         handle = mock_open()
         handle.write.assert_called_once_with('this is a test')
+
+    @mock.patch.object(pkg_update_deps.os.path, 'isfile')
+    def test_find_package_line_in_req_file(self, _isfile):
+        _isfile.return_value = True
+        open_name = '%s.open' % 'pkg_update_deps'
+        requirements_text = """
+python3-eventlet (>= 0.18.2)
+python3-pecan (>= 1.3.2)
+"""
+        with mock.patch(
+                open_name, mock.mock_open(read_data=requirements_text)):
+            pkg = pkg_update_deps.find_package_line_in_req_file(
+                'test', 'python3-pecan')
+        self.assertEqual(pkg, "python3-pecan (>= 1.3.2)\n")
+
+    def test_swap_package_name_and_min_version_with_min_version(self):
+        res = pkg_update_deps.swap_package_name_and_min_version(
+            'pecan>=1.3.2', 'pecan', 'python3-pecan', True
+        )
+        self.assertEqual(res, 'python3-pecan (>= 1.3.2)')
+
+    def test_swap_package_name_and_min_version_without_min_version(self):
+        res = pkg_update_deps.swap_package_name_and_min_version(
+            'pecan', 'pecan', 'python3-pecan', False
+        )
+        self.assertEqual(res, 'python3-pecan')
+
+    def test_process_requirements_files_phase1(self):
+        pass
+
+    def test_process_requirements_files_phase2(self):
+        pass
+
+    def test_process_control_file_phase1(self):
+        pass
+
+    def test_process_control_file_phase2(self):
+        pass
